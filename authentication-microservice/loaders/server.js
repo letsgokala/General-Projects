5a196b5a8367b15c0bf624d031daac8eaddfe4c4
Khalid <nwakala730@gmail.com>
2025-12-29 21:49:53 +0300

    add login routes

--- authentication-microservice/loaders/server.js
+++ authentication-microservice/loaders/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const healtheController = require("../controllers/health")
+const fallbackController = require("../controllers/fallback")
+const loginController = require("../controllers/login")
+const signUpController = require("../controllers/signup")
 
 module.exports = (env) => {
     const app = express();
 
     app.get("/health", healtheController);
+
+    app.post("/auth/login", loginController)
+
+    app.post("/auth/signup", signUpController)
+
+    app.use(fallbackController)
 
     return app;
 }
