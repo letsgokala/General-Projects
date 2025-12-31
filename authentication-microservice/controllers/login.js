4aaad0e685821596c35123d54d3176887363adcd
Khalid <nwakala730@gmail.com>
2025-12-31 03:28:46 +0300

    integrate schemas to the controllers

--- authentication-microservice/controllers/login.js
+++ authentication-microservice/controllers/login.js
@@ -1,4 +1,17 @@
-module.exports = (req, res) => {
-    console.log(req.body);    
-    res.sendStatus(200);
+const login = require("../services/login");
+
+module.exports = (env) => async (req, res) => {
+try {
+    const token = await login(req.body, env.jwt.secret);
+    if (token){
+        res.json({ token })
+    } else {
+        res.sendStatus(401);
+    }
+    
+} catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+    
+}
 }
--- authentication-microservice/controllers/signup.js
+++ authentication-microservice/controllers/signup.js
@@ -1,4 +1,11 @@
-module.exports = (req, res) => {
-    console.log(req.body);
-    res.statusCode(200);
+const signup = require("../services/signup");
+
+module.exports = async (req, res) => {
+try {
+    await signup(req.body)
+    res.sendStatus(201);
+} catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+}
 }
