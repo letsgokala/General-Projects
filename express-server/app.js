c792e24dee52aa5333b432c8ef4b8a1c9d3822a0
Khalid <nwakala730@gmail.com>
2026-01-01 19:33:03 +0300

    add post endpoint in express

--- express-server/app.js
+++ express-server/app.js
@@ -7,6 +7,19 @@ app.get("/hello", (req, res) => {
     res.send("hello world");
 });
 
+app.post("/login", express.json(), (req, res) => {
+    const { email, password } = req.body;
+
+    if (email == "user@mail.com" && password === "password"){
+        res.json({
+            token: "access_token"
+        }); 
+    } else {
+        res.status(401).json({
+            error: "Invalid_credentials"
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);    
