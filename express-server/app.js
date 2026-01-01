25a6347ba5bd3d48251176b5b597f931464d928f
Khalid <nwakala730@gmail.com>
2026-01-01 19:28:06 +0300

    add first endpoint in express

--- express-server/app.js
+++ express-server/app.js
@@ -1,4 +1,3 @@
-const { log } = require("console");
 const express = require("express");
 
 const app = express();
@@ -8,19 +7,6 @@ app.get("/hello", (req, res) => {
     res.send("hello world");
 });
 
-app.post("/login", express.json(), (req, res) => {
-    const { email, password } = req.body;
-
-    if (email == "user@mail.com" && password === "password"){
-        res.json({
-            token: "access_token"
-        }); 
-    } else {
-        res.status(401).json({
-            error: "Invalid_credentials"
-        });
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);    
